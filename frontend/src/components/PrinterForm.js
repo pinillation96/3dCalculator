18770c957de3a4c98a1707503e5157446a9f2bc7
pinillation96 <44420238+pinillation96@users.noreply.github.com>
2024-07-13 14:18:14 -0600

    review

--- frontend/src/components/PrinterForm.js
+++ frontend/src/components/PrinterForm.js
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addPrinter } from '../redux/actions/printerActions';
 
 const PrinterForm = () => {
+  const [brand, setBrand] = useState('');
+  const [model, setModel] = useState('');
+  const [electricityCost, setElectricityCost] = useState('');
+  const [modifications, setModifications] = useState('');
+  const [nozzles, setNozzles] = useState('');
+  const [bedType, setBedType] = useState('');
+  const [bedSize, setBedSize] = useState('');
+  const [extruderType, setExtruderType] = useState('');
+  const [purchasePrice, setPurchasePrice] = useState('');
+
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(addPrinter({
+      brand,
+      model,
+      electricityCost,
+      modifications,
+      nozzles,
+      bedType,
+      bedSize,
+      extruderType,
+      purchasePrice,
+    }));
+  };
+
   return (
-    <form>
-      <input type="text" placeholder="Marca" />
-      <input type="text" placeholder="Modelo" />
-      <input type="number" placeholder="Gasto de electricidad" />
-      <input type="text" placeholder="Modificaciones" />
-      <input type="text" placeholder="Boquillas" />
-      <input type="text" placeholder="Tipo de cama" />
-      <input type="text" placeholder="Tamaño de la cama" />
-      <input type="text" placeholder="Tipo de extrusora" />
-      <input type="number" placeholder="Precio de compra" />
-      <button type="submit">Agregar Impresora</button>
+    <form onSubmit={submitHandler}>
+      <input type="text" placeholder="Brand" value={brand} onChange={(e) => setBrand(e.target.value)} required />
+      <input type="text" placeholder="Model" value={model} onChange={(e) => setModel(e.target.value)} required />
+      <input type="number" placeholder="Electricity Cost" value={electricityCost} onChange={(e) => setElectricityCost(e.target.value)} required />
+      <input type="text" placeholder="Modifications" value={modifications} onChange={(e) => setModifications(e.target.value)} />
+      <input type="text" placeholder="Nozzles" value={nozzles} onChange={(e) => setNozzles(e.target.value)} />
+      <input type="text" placeholder="Bed Type" value={bedType} onChange={(e) => setBedType(e.target.value)} />
+      <input type="text" placeholder="Bed Size" value={bedSize} onChange={(e) => setBedSize(e.target.value)} required />
+      <input type="text" placeholder="Extruder Type" value={extruderType} onChange={(e) => setExtruderType(e.target.value)} />
+      <input type="number" placeholder="Purchase Price" value={purchasePrice} onChange={(e) => setPurchasePrice(e.target.value)} required />
+      <button type="submit">Add Printer</button>
     </form>
   );
 };
 
 export default PrinterForm;
+
--- frontend/src/redux/actions/printerActions.js
+++ frontend/src/redux/actions/printerActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { PRINTER_LIST_SUCCESS, PRINTER_LIST_FAIL } from '../constants/printerConstants';
+import { PRINTER_LIST_SUCCESS, PRINTER_LIST_FAIL, ADD_PRINTER_SUCCESS, ADD_PRINTER_FAIL } from '../constants/printerConstants';
 
 export const listPrinters = () => async (dispatch) => {
   try {
@@ -8,6 +8,18 @@ export const listPrinters = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PRINTER_LIST_FAIL,
+      payload: error.response && error.response.data.message ? error.response.data.message : error.message,
+    });
+  }
+};
+
+export const addPrinter = (printer) => async (dispatch) => {
+  try {
+    const { data } = await axios.post('/api/printers', printer);
+    dispatch({ type: ADD_PRINTER_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: ADD_PRINTER_FAIL,
       payload: error.response && error.response.data.message ? error.response.data.message : error.message,
     });
   }
