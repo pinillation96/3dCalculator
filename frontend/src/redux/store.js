dd66794cc65b0438a3f1288acf4b32d2679142c0
pinillation96 <44420238+pinillation96@users.noreply.github.com>
2024-07-13 12:11:23 -0600

    frontUp

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import printerReducer from './reducers/printerReducer';
 import materialReducer from './reducers/materialReducer';
